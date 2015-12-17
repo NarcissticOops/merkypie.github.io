efb9c9c7a8e6a87e33101e5784ab5ed3bb549743
Merkypie <tscooper@fullsail.edu>
2015-12-17 11:53:48 -0500

    jquery script

--- tumblr/public/b2classic/js/scripts.js
+++ tumblr/public/b2classic/js/scripts.js
@@ -1,6 +1,6 @@
 $('document').ready(function(){
   $('iframe').load(function(){
     $('iframe').contents().find('head').append($(
-      '<style type="text/css"> .photoset_row {width: 590px; !important margin: 0 auto; !important } .photoset_row img { height: auto; !important} </style>'));
+      '<link href="http://merkypie.github.io/tumblr/public/b2classic/css/style.css" rel="stylesheet" type="text/css">'));
   });
 });
